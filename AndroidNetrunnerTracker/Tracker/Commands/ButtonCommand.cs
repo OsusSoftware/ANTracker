b7478e27676fbf6bb6533d61451864e840af7da7
ElOsus <raul.gonzalez@live.com>
2015-03-15 22:18:05 -0700

    -Adding Button Command

---
+++ AndroidNetrunnerTracker/Tracker/Commands/ButtonCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Tracker.Commands
+{
+    public class ButtonCommand : ICommand
+    {
+        Action<object> _whatToExecute;
+        Func<object, bool> _whenToExecute;
+
+        public ButtonCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this._whatToExecute = execute;
+            this._whenToExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            bool result = false;
+
+            if (_whenToExecute != null)
+            {
+                result = this._whenToExecute(parameter);
+            }
+
+            return result;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            this._whatToExecute(parameter);
+        }
+    }
+}
